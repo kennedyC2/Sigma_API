5489a791a48dc2624cb1a2d46ffb57af5c4283a3
kennedyC2 <kennedychidi55@gmail.com>
2023-12-27 21:20:26 +0100

    xxx

--- index.js
+++ index.js
@@ -5,8 +5,6 @@
 // =======================================================
 const { SERVER_init } = require("./lib/server");
 const { CLI_init } = require("./lib/cli");
-const { delete_token } = require("./lib/worker");
-const { hour } = require("./lib/helper");
 
 // Declare APP
 // =======================================================
@@ -20,17 +18,7 @@ app["init"] = () => {
     setTimeout(() => {
         // Initialize CLI
         CLI_init();
-
-        // Token
-        delete_token();
     }, 1000);
-
-    setInterval(() => {
-        // Token
-        if (parseInt(hour) === 22) {
-            delete_token();
-        }
-    }, 1000 * 60 * 60);
 };
 
 // Kick
